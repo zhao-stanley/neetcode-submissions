c9c864719e79dcc6dc8f7a02af6e8e0e0e73c2ce
Stanley Zhao <stanleyhzhao@gmail.com>
2026-06-06 19:51:50 -0400

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+        unicode_offset = 97
+
+        for st in strs:
+            bits = 26 * [0]
+            for char in st:
+                bit = ord(char) - unicode_offset
+                bits[bit] += 1
+            if str(bits) not in seen:
+                seen[str(bits)] = [st]
+            else:
+                seen[str(bits)].append(st)
+            print(bits)
+
+        return [value for value in seen.values()]
+
+
+
+
