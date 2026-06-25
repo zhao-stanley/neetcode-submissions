9dfc3c37f701e61ba7e6150d12bffc093f112b2d
Stanley Zhao <stanleyhzhao@gmail.com>
2026-06-24 23:01:38 -0400

    Add: counting-bits - submission-6

---
+++ Data Structures & Algorithms/counting-bits/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = {}
+        output = []
+        def helper(i: int):
+            # base cases
+            if i == 0 or i == 1:
+                # memoize
+                if i not in dp:
+                    dp[i] = i
+                return i
+            
+            # an int n is the same as n >> 1 + lsb(n) (0 or 1)
+            bits = helper(i >> 1) + helper(i & 1)
+            
+            # memoize our calculations
+            if i not in dp:
+                dp[i] = bits
+
+            return bits
+
+        # calculate for each integer from [0,n]
+        return [helper(i) for i in range(n+1)]
