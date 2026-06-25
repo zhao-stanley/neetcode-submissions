1f943c0d5db2547439feda1e77b6faa9a53d03a3
Stanley Zhao <stanleyhzhao@gmail.com>
2026-06-24 22:43:29 -0400

    Update: counting-bits - submission-0

--- Data Structures & Algorithms/counting-bits/submission-0.py
+++ Data Structures & Algorithms/counting-bits/submission-0.py
@@ -1,10 +1,10 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        output = []
-        for i in range(n+1):
-            num_ones = 0
-            binary_num = bin(i)[2:]
-            for dig in binary_num:
-                num_ones += int(dig)
-            output.append(num_ones)
-        return output
+        dp = []
+        
+        def helper(i: int):
+            if i == 0 or i == 1:
+                return i
+            return helper(i >> 1) + helper(i & 1)
+
+        return [helper(i) for i in range(n+1)]    
