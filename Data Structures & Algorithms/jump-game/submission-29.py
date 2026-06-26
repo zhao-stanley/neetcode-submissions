c9e43889a664445cf24164afe251bae6f6acf668
Stanley Zhao <stanleyhzhao@gmail.com>
2026-06-26 12:11:39 -0400

    Add: jump-game - submission-29

---
+++ Data Structures & Algorithms/jump-game/submission-29.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        i = 0
+
+        while i < len(nums):
+            jl = nums[i]
+            if jl == 0 and i < len(nums) - 1:
+                return False
+            subset = nums[i+1:i+1+jl]
+            max_jl = (0, 1)
+            for offset in range(1, jl+1):
+                resultant = i + offset + nums[min(i + offset, len(nums) - 1)]
+                if resultant > max_jl[0]:
+                    max_jl = resultant, offset
+
+            # for offset, num in enumerate(subset):
+            #     resultant = i + offset + 1 + num
+            #     if resultant > max_jl[0]:
+            #         max_jl = resultant, offset + 1
+                    
+            i += max_jl[1]
+            
+        return i >= len(nums)
