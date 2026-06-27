b83b3671f7461003f21461fd16b21493af88cefb
Stanley Zhao <stanleyhzhao@gmail.com>
2026-06-27 16:27:51 -0400

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        if not strs:
+            return ""
+        for s in strs:
+            encoded += f"{len(s)}#{s}"
+        print(encoded)
+        return encoded
+    
+    def decode(self, s: str) -> List[str]:
+        if s == "":
+            return []
+        decoded = []
+        i=0
+        while i < len(s):
+            length = ""
+            j = i
+            while s[j] != "#":
+                length += s[j]
+                j += 1
+            eow = j + 1 + int(length)
+            decoded_chunk = s[j + 1:eow]
+            decoded.append(decoded_chunk)
+            i = eow
+
+        return decoded
+
+
